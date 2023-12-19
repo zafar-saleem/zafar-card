9694665a551ed5af7ea28f565aec444b6d98feb8
Zafar Saleem <zafarsaleem3@gmail.com>
2023-12-19 19:07:19 +0500

    fix(style): fixed height unit

--- components/center/center.styled.tsx
+++ components/center/center.styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Center = styled.main`
   width: 100vw;
-  height: 100vh;
+  height: 100dvh;
   display: flex;
   justify-content: center;
   align-items: center;
