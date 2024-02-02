9d3e59e216ed6c4132aecc99ef904377545367e0
Zafar Saleem <zafarsaleem3@gmail.com>
2024-02-02 11:57:08 +0500

    chore: updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Providers from "./styled-providers";
 import GoogleAnalytics from './GoogleAnalytics';
 
 export const metadata: Metadata = {
-  title: "Zafar Saleem | JavaScript | React | React Hooks | Redux | Node | Express | MongoDB",
+  title: "Zafar Saleem | JavaScript | React | React Hooks | Redux | Node | Express | MongoDB | Freelance | Freelance front end developer | Freelance Web Developer",
   description: "This is Zafar Saleem personal site where you can find his technical/software engineering skills and links to his Medium blogs, Github profile, Gitlab profile, Linkedin profile, Instagram profile, link to his CV/Resume in .pdf format.",
 }
 
